a2daf9c1d2c7d5ad38b777afa2cb71f51884c2cb
Robin Niel <robin.niel@brut.media>
2024-02-03 21:16:54 +0100

    WIP

--- GPIO.py
+++ GPIO.py
@@ -4,3 +4,5 @@ from config import relay_lower_pin_num, relay_raise_pin_num
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(relay_lower_pin_num, GPIO.OUT)
 GPIO.setup(relay_raise_pin_num, GPIO.OUT)
+GPIO.output(relay_lower_pin_num, 0)
+GPIO.output(relay_raise_pin_num, 0)
