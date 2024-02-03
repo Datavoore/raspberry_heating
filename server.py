0fea2e2b2129b1bf71cbb1a84616d452acb928ea
Robin Niel <robin.niel@brut.media>
2024-02-03 20:39:16 +0100

    WIP

--- server.py
+++ server.py
@@ -11,7 +11,6 @@ from config import favicon_path
 from fastapi.responses import FileResponse
 from fastapi import BackgroundTasks
 
-app = FastAPI()
 heating_controller = HeatingController(sondes[2], None, Valve(relay_lower_pin_num, relay_raise_pin_num), 50)
 
 
@@ -24,9 +23,10 @@ def run_main_loop():
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Initialize heater controller and run background tasks
-    main_loop = asyncio.create_task(run_main_loop())
+    asyncio.run(run_main_loop())
     yield
 
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/temperature/{sonde_number}")
 async def get_temp(sonde_number: int):
