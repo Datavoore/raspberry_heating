cabdfd0a26fd823ec32cc91439e4e58e9a10f3a4
Robin Niel <robin.niel@brut.media>
2024-04-01 17:05:07 +0200

    WIP

--- restart_script.py
+++ restart_script.py
@@ -10,8 +10,10 @@ for process in psutil.process_iter():
         found_main = True
 
 if not found_serv:
+    print("Server not running, starting it")
     subprocess.Popen(['nohup', 'uvicorn', '--app-dir',  '/home/pi/Documents/raspberry_heating', 'server:app','--reload', '--host', '0.0.0.0', '--port', '8042'])
 if not found_main:
+    print("Main not running, starting it")
     subprocess.Popen(
         ['nohup', 'python3', '/home/pi/Documents/raspberry_heating/main.py', '>', '/home/pi/logs/main_py_logs.txt',
          '&'])
